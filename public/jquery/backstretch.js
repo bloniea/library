20eaac98fdc45d39eb8a685d76972a411f2b4558
bloniea <bloniea@qq.com>
2023-08-21 16:56:07 +0800

    add

--- public/jquery/backstretch.js
+++
@@ -1,9 +0,0 @@
-$('#bg').backstretch([
-  { url: 'https://images.bloniea.com/images1/105992683_p0.png', fade: 550 },
-
-], { duration: 6000 })
-  .css({
-    position: 'fixed',
-    opacity: '.8'
-  })
-
